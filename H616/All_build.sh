23dedf24005424df59a39f3223450a1932caad66
lodge <shilong.native@gmail.com>
2022-03-26 10:02:45 +0800

    update

--- H616/All_build.sh
+++ H616/All_build.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 Pi_user=orangepi
-Pi_IP=192.168.0.239
+Pi_IP=192.168.0.96
 
 cd /home/lodge/Allwinner/orangepi-build
+
+cp ./u-boot/v2021.07-sunxi/.config ./u-boot/v2021.07-sunxi/configs/orangepi_zero2_defconfig
 
 sudo ./build.sh
 
 echo -e "\n ==== copy images ====\n"
+scp output/debs/u-boot/linux-*.deb $Pi_user@$Pi_IP:/home/$Pi_user
 scp output/debs/linux-*.deb $Pi_user@$Pi_IP:/home/$Pi_user
 
 echo -e "\n ==== copy update script ====\n"
-scp $SHELL_ROOT_PATH/Orangepi-H616/update_kernel.sh $Pi_user@$Pi_IP:/home/$Pi_user
+scp $SHELL_ROOT_PATH/H616/update_kernel.sh $Pi_user@$Pi_IP:/home/$Pi_user
 
 echo -e "\n **** build complete! ****\n"
 
---
+++ H616/update_kernel.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cd ~
+
+sudo sed -i "s/verbosity=1/verbosity=7/" /boot/orangepiEnv.txt
+
+if [ -f "./linux-u-boot-current-orangepizero2_2.1.8_arm64.deb" ];then
+    echo -e "\n **** remove uboot ****\n"
+    sudo apt purge -y linux-u-boot-orangepizero2-current
+    echo -e "\n **** install uboot ****\n"
+    sudo dpkg -i linux-u-boot-current-orangepizero2_2.1.8_arm64.deb
+
+    sudo nand-sata-install
+fi
+
+if [ -f "./linux-dtb-current-sun50iw9_2.1.8_arm64.deb" ];then
+    echo -e "\n **** remove dtb ****\n"
+    sudo apt purge -y linux-dtb-current-sun50iw9
+    echo -e "\n **** install dtb ****\n"
+    sudo dpkg -i linux-dtb-current-sun50iw9_2.1.8_arm64.deb
+fi
+
+if [ -f "./linux-image-current-sun50iw9_2.1.8_arm64.deb" ];then
+    echo -e "\n **** remove kernel ****\n"
+    sudo apt purge -y linux-image-current-sun50iw9
+    echo -e "\n **** install kernel ****\n"
+    sudo dpkg -i linux-image-current-sun50iw9_2.1.8_arm64.deb
+fi
+
+sudo rm ./linux-*.deb
+
+sudo reboot
+
