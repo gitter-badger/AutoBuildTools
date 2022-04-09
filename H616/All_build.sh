db8dcc44e325f126dbda7809e41f987189a9fd72
lodge <shilong.native@gmail.com>
2022-04-09 16:31:01 +0800

    f

--- H616/All_build.sh
+++ H616/All_build.sh
@@ -7,14 +7,52 @@ cd /home/lodge/Allwinner/orangepi-build
 
 cp ./u-boot/v2021.07-sunxi/.config ./u-boot/v2021.07-sunxi/configs/orangepi_zero2_defconfig
 
+#=================================
+
+
+echo -e "\n ==== copy update script ====\n"
+cp $SHELL_ROOT_PATH/H616/update_kernel.sh /home/lodge/Allwinner/orangepi-build/nfs_folder/
+
+
+
+echo -e "\n ==== copy wifi files ====\n"
+
+#cd /home/lodge/Allwinner/orangepi-build/nfs_folder/29_WIFI
+#cp regulatory.* /home/lodge/Allwinner/orangepi-build/nfs_folder/
+# scp regulatory.db regulatory.db.p7s $Pi_user@$Pi_IP:/home/$Pi_user
+
+cd ~/Allwinner/orangepi-build/kernel/orange-pi-5.13-sunxi64/drivers/net/wireless/rtl8189fs
+#cp 8189fs.ko /home/lodge/Allwinner/orangepi-build/nfs_folder/
+
+cd ~/Allwinner/orangepi-build/kernel/orange-pi-5.13-sunxi64/net/wireless/
+#cp cfg80211.ko /home/lodge/Allwinner/orangepi-build/nfs_folder/
+
+
+cd /home/lodge/Allwinner/orangepi-build
 sudo ./build.sh
 
 echo -e "\n ==== copy images ====\n"
-scp output/debs/u-boot/linux-*.deb $Pi_user@$Pi_IP:/home/$Pi_user
-scp output/debs/linux-*.deb $Pi_user@$Pi_IP:/home/$Pi_user
 
-echo -e "\n ==== copy update script ====\n"
-scp $SHELL_ROOT_PATH/H616/update_kernel.sh $Pi_user@$Pi_IP:/home/$Pi_user
+cp output/debs/u-boot/linux-*.deb /home/lodge/Allwinner/orangepi-build/nfs_folder/
+cp output/debs/linux-*.deb /home/lodge/Allwinner/orangepi-build/nfs_folder/
+
+echo -e "\n ==== copy modules ====\n"
+cd /home/lodge/Allwinner/orangepi-build/nfs_folder/
+
+rm -fr 5.13.0-sun50iw9/
+
+cd ~/Allwinner/orangepi-build/kernel/orange-pi-5.13-sunxi64/debian/tmp/lib/modules
+
+cp -r 5.13.0-sun50iw9/ /home/lodge/Allwinner/orangepi-build/nfs_folder/
+
+
+echo -e "\n ==== copy images ====\n"
+cd /home/lodge/Allwinner/orangepi-build/nfs_folder/
+
+#scp -r 5.13.0-sun50iw9 *.ko *.deb regulatory.* *.sh $Pi_user@$Pi_IP:/home/$Pi_user
+
+scp -r 5.13.0-sun50iw9 *.deb regulatory.* *.sh $Pi_user@$Pi_IP:/home/$Pi_user
 
 echo -e "\n **** build complete! ****\n"
+
 
--- H616/update_kernel.sh
+++ H616/update_kernel.sh
@@ -2,15 +2,23 @@
 
 cd ~
 
+sudo apt install udhcpc -y
+
+sudo mv regulatory.db* /lib/firmware/
+
+sudo rm /lib/modules/5.13.0-sun50iw9 -fr
+sudo mv ./5.13.0-sun50iw9 /lib/modules/ -f
+
+
 sudo sed -i "s/verbosity=1/verbosity=7/" /boot/orangepiEnv.txt
 
 if [ -f "./linux-u-boot-current-orangepizero2_2.1.8_arm64.deb" ];then
     echo -e "\n **** remove uboot ****\n"
-    sudo apt purge -y linux-u-boot-orangepizero2-current
+#    sudo apt purge -y linux-u-boot-orangepizero2-current
     echo -e "\n **** install uboot ****\n"
-    sudo dpkg -i linux-u-boot-current-orangepizero2_2.1.8_arm64.deb
+#    sudo dpkg -i linux-u-boot-current-orangepizero2_2.1.8_arm64.deb
 
-    sudo nand-sata-install
+#    sudo nand-sata-install
 fi
 
 if [ -f "./linux-dtb-current-sun50iw9_2.1.8_arm64.deb" ];then
